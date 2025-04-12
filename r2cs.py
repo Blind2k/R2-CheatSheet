2f4f7ddade72a22d98da479a7666aa69f3206646
Blind2k <94520776+Blind2k@users.noreply.github.com>
2025-04-12 18:59:41 +0700

    Update r2cs.py
    minor adjustments 08

--- r2cs.py
+++ r2cs.py
@@ -4,6 +4,12 @@ from modules.local_classes import LocalHostConfigurations as LocalH
 from modules.remote_classes import RemoteHostConfigurations as RemoteH
 from handlers.navigate_user_by_input import take_user_input_round
 from handlers.additional_communication import load_show_menu
+import os
+
+# Get the directory where r2cs.py lives
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+# Move to the project root
+os.chdir(SCRIPT_DIR)
 
 
 # The main function that should last forever. This is where the interactive part repeats itself
